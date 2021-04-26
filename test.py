a125a974cb60a4b7a34a5eeca3d55ba5a35e6fea
Naimi Malek <80359535+MalekNaimi@users.noreply.github.com>
2021-04-26 05:10:36 +0200

    Update test.py

--- test.py
+++ test.py
@@ -6,15 +6,15 @@ from keras.models import load_model
 from utility import functions, globalvars
 import librosa
 import numpy as np
-from train import U_test, X_test
+from train import y_test, y_test
 
 emotion_classes=['anger','boredom','disgust','anxiety/fear','happiness','sadness','neutral']
 
 
 def test(model_path:str):
     model=load_model(model_path)
-    Y_pred = model.predict([U_test, X_test])
-    matrix = confusion_matrix(Y_test.argmax(axis=1), Y_pred.argmax(axis=1))
+    y_pred = model.predict([u_test, x_test])
+    matrix = confusion_matrix(y_test.argmax(axis=1), y_pred.argmax(axis=1))
     ax = sns.heatmap(matrix, annot=True, fmt="d", cmap = 'rocket_r', emotion_classes=['anger','boredom','disgust','anxiety/fear','happiness','sadness','neutral'])
     return matrix
     
