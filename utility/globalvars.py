c32f096bbcb4a7ead2865c3f13f5f5986a25cf4a
Naimi Malek <80359535+MalekNaimi@users.noreply.github.com>
2021-05-04 13:24:18 +0200

    Update globalvars.py

--- utility/globalvars.py
+++ utility/globalvars.py
@@ -8,7 +8,7 @@ attention_init_value = 1.0 / 256
 nb_hidden_units = 512   # number of hidden layer units
 dropout_rate = 0.5
 nb_lstm_cells = 128
-nb_classes = 4
+nb_classes = 7
 
 frame_size = 0.025  # 25 msec segments
 step = 0.01     # 10 msec time step
